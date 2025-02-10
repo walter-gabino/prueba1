2e8cf61c0c49f381733eea0dfe0cc2aff6f2c244
walter-gabino <walter.gabino@gmail.com>
2025-02-10 17:40:16 -0600

    tabla de multiplicar

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@ using namespace std;
 
 int main(){
 
-    cout<<"coweee";
+    cout<<"coweee"<<endl;
+    int i=1;
+    int multiplicacion=1;
+    int numero;
+    cout<<"ingresa un numero entero"<<endl;
+    cin>>numero;
+
+    for(i=1; i<=10; i++){
+        multiplicacion=i*numero;
+        cout<<numero<<" * "<<i<<"="<<multiplicacion<<endl;
+    }
+
     return 0;
 }
