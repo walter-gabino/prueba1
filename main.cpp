d1cb2ae7daa3eeba5463d37b8d1f4154f1b56224
walter-gabino <walter.gabino@gmail.com>
2025-02-10 18:07:44 -0600

    tablas de multiplicar

--- main.cpp
+++ main.cpp
@@ -3,17 +3,35 @@ using namespace std;
 
 int main(){
 
-    cout<<"coweee"<<endl;
     int i=1;
     int multiplicacion=1;
     int numero;
-    cout<<"ingresa un numero entero"<<endl;
-    cin>>numero;
-
+    /*
     for(i=1; i<=10; i++){
+        numero=4;
+        multiplicacion=i*numero;
+        cout<<numero<<" * "<<i<<"="<<multiplicacion<<endl;
+
+    }
+    cout<<endl;
+    for(i=1; i<=10; i++){
+        numero=6;
         multiplicacion=i*numero;
         cout<<numero<<" * "<<i<<"="<<multiplicacion<<endl;
     }
 
+     */
+    int factorial;
+    cout<<"ingrese el numero del que desea el factorial: "<<endl;
+    cin>>factorial;
+    int multiplicacionFactorial=1;
+    int resultado=0;
+
+    for (int j = factorial; j=1 ; --j) {
+        multiplicacionFactorial=i*factorial;
+        resultado=+multiplicacionFactorial;
+    }
+
+    cout<<"el resultado es: "<<resultado;
     return 0;
 }
